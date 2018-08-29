3fce8ad2074332fd622184d00377d15d07e2e696
Steven <codebusiness2014@gmail.com>
2018-08-28 23:36:06 -0400

    Requiring express-graphql

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
 
 const app = express();
+
+const app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
