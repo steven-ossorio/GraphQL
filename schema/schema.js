973017c9fe08648c48595be9bb8d00906e33fa2d
Steven <codebusiness2014@gmail.com>
2018-09-01 22:00:54 -0400

    added updateUser to the mutation

--- schema/schema.js
+++ schema/schema.js
@@ -107,6 +107,20 @@ const mutation = new GraphQLObjectType({
           .delete(`http://localhost:3000/users/${id}`)
           .then(res => res.data);
       }
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => res.data);
+      }
     }
   }
 });
