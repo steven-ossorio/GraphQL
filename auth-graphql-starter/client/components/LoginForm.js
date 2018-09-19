ae30c03f56609592cdbab9500748e899eb12736d
Steven <codebusiness2014@gmail.com>
2018-09-18 22:20:47 -0400

    Added componentWIllUpdate to loginform which redirects a user who is logged in

--- auth-graphql-starter/client/components/LoginForm.js
+++ auth-graphql-starter/client/components/LoginForm.js
@@ -3,6 +3,7 @@ import AuthForm from "./AuthForm";
 import mutation from "../mutations/Login";
 import { graphql } from "react-apollo";
 import query from "../queries/CurrentUser";
+import { hashHistory } from "react-router";
 
 class LoginForm extends Component {
   constructor(props) {
@@ -12,6 +13,13 @@ class LoginForm extends Component {
       errors: []
     };
   }
+
+  componentDidUpdate(nextProps) {
+    if (!this.props.data.user && nextProps.data.user) {
+      hashHistory.push("/dashboard");
+    }
+  }
+
   onSubmit({ email, password }) {
     this.props
       .mutate({
@@ -40,4 +48,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(query)(graphql(mutation)(LoginForm));
